5f2788b44e9bdba23cd5f836ea3b4e27e38efdf5
razvan404 <razvan.petec@sectorlabs.ro>
2025-04-19 13:50:39 +0300

    Add console render engine

---
+++ src/render_engines/__init__.py
@@ -0,0 +1,4 @@
+from .console_render_engine import ConsoleRenderEngine
+from .pygame_render_engine import PygameRenderEngine
+
+__all__ = ["ConsoleRenderEngine", "PygameRenderEngine"]
---
+++ src/render_engines/console_render_engine.py
@@ -0,0 +1,32 @@
+from src.interfaces.render_engine import RenderEngine
+
+from src.state import GameState
+
+
+class ConsoleRenderEngine(RenderEngine):
+    SYMBOLS = {"wall": "#", "empty": " ", "dead": "x"}
+
+    def display(self, state: GameState):
+        grid = [
+            [self.SYMBOLS["empty"] for _ in range(state.map.width)]
+            for _ in range(state.map.height)
+        ]
+
+        # Place walls
+        for x, y in state.map.walls:
+            grid[y][x] = self.SYMBOLS["wall"]
+
+        # Place agents
+        for agent in state.agents.values():
+            agent_data = agent.map_data
+            x, y = agent_data.position
+            if not agent.is_alive:
+                symbol = self.SYMBOLS["dead"]
+            else:
+                symbol = agent_data.team.lower()
+            grid[int(y)][int(x)] = symbol
+
+        print(f"Tick: {state.tick}")
+        for row in grid:
+            print("".join(row))
+        print()
