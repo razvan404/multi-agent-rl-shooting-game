fc536e42ad11bb41fa921df4265bb07a5a0f92b4
razvan404 <razvan.petec@sectorlabs.ro>
2025-04-19 17:45:17 +0300

    Stop simulation through the render engine when certain event is triggered

--- src/render_engines/__init__.py
+++ src/render_engines/__init__.py
@@ -1,4 +1,5 @@
 from .console_render_engine import ConsoleRenderEngine
 from .pygame_render_engine import PygameRenderEngine
+from .exceptions import StopSimulationException
 
-__all__ = ["ConsoleRenderEngine", "PygameRenderEngine"]
+__all__ = ["ConsoleRenderEngine", "PygameRenderEngine", "StopSimulationException"]
